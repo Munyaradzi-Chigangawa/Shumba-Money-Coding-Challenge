e88e065842ace9472694b5b4dc798b48cdd662bd
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-09-01 18:39:22 +0200

    changed data type

--- backend/backend/src/main/java/com/shumbamoney/backend/repo/recipientRepo.java
+++ backend/backend/src/main/java/com/shumbamoney/backend/repo/recipientRepo.java
@@ -2,5 +2,5 @@ package com.shumbamoney.backend.repo;
 import com.shumbamoney.backend.models.recipient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface recipientRepo extends JpaRepository <recipient, String> {
+public interface recipientRepo extends JpaRepository <recipient, Long> {
 }
