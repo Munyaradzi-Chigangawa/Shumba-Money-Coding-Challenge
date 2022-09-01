a7e864fa653de04de688486b28b9c1674a279e89
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-09-01 18:39:39 +0200

    added CRUD Operations

--- backend/backend/src/main/java/com/shumbamoney/backend/controllers/recipientController.java
+++ backend/backend/src/main/java/com/shumbamoney/backend/controllers/recipientController.java
@@ -5,9 +5,12 @@ import com.shumbamoney.backend.models.recipient;
 import com.shumbamoney.backend.services.recipientService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/")
@@ -23,5 +26,26 @@ public class recipientController {
         log.info("Recipient Added.");
         return new ResponseEntity<>(_recipientService.save(recipient), HttpStatus.CREATED);
     }
+
+    @GetMapping( value = "/getRecipients")
+    public ResponseEntity<List<recipient>> getRecipients() {
+        log.info("Recipients Retrieved.");
+        return new ResponseEntity<>(_recipientService.getRecipients(), HttpStatus.OK);
+    }
+
+    @DeleteMapping( value = "/deleteRecipient/{id}")
+    public ResponseEntity<recipient> deleteRecipient (@PathVariable Long id) {
+        log.info("Recipient Deleted.");
+        return new ResponseEntity<>(_recipientService.deleteRecipient(id), HttpStatus.OK);
+    }
+
+    @GetMapping( value = "/getRecipient/{id}")
+    public ResponseEntity<recipient> getRecipient(@PathVariable Long id) {
+        log.info("Recipient Retrieved");
+        return new ResponseEntity<>(_recipientService.getRecipient(id), HttpStatus.OK);
+    }
+
+//    @PutMapping( value = "/updateRecepient/{id}")
+
 
 }
