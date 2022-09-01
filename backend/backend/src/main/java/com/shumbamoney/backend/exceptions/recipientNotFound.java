7ac653c50eb2518d8159952fb9955d9792b2d628
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-09-01 15:48:12 +0200

    added exception

---
+++ backend/backend/src/main/java/com/shumbamoney/backend/exceptions/recipientNotFound.java
@@ -0,0 +1,8 @@
+package com.shumbamoney.backend.exceptions;
+
+public class recipientNotFound extends RuntimeException {
+        public recipientNotFound(String message) {
+            super(message);
+        }
+    }
+}
