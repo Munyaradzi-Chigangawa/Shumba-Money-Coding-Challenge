a56a930dc4c9abcc79ddd944c174f386b95e0e52
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-09-01 08:40:39 +0200

    added models and database configuration files

---
+++ backend/backend/src/main/java/com/shumbamoney/backend/models/recipient.java
@@ -0,0 +1,28 @@
+package com.shumbamoney.backend.models;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recipients")
+@Builder
+
+public class recipient {
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(length = 100)
+    private Long recipientId;
+    @Column(name = "recipient_name", columnDefinition = "varchar(255)")
+    private String recipientName;
+    private String recipientEmail;
+    private String recipientAddress;
+    private String recipientCell;
+    private String recipientCountry;
+    private  String recipientTown;
+
+}
---
+++ backend/backend/src/main/java/com/shumbamoney/backend/models/sender.java
@@ -0,0 +1,29 @@
+package com.shumbamoney.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "senders")
+@Builder
+public class sender {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(length = 100)
+    private Long senderId;
+    @Column(name = "sender_name", columnDefinition = "varchar(255)")
+    private String senderName;
+    private String senderEmail;
+    private String senderAddress;
+    private String password;
+    private String senderCell;
+    private String senderCountry;
+    private  String senderTown;
+}
