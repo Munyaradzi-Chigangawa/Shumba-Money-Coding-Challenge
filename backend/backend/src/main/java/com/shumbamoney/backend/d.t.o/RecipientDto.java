346fa6de65e543a249c02aca4a8e39b85f312689
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-09-03 15:53:44 +0200

    added validation

--- backend/backend/src/main/java/com/shumbamoney/backend/d.t.o/RecipientDto.java
+++ backend/backend/src/main/java/com/shumbamoney/backend/d.t.o/RecipientDto.java
@@ -1,16 +1,22 @@
 package com.shumbamoney.backend.d.t.o;
 import lombok.*;
-
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class RecipientDto implements Serializable {
+    @NotEmpty(message = "Name Required.")
     private String recipientName;
+    @NotEmpty(message = "Email Required.")
     private String recipientEmail;
+    @NotEmpty(message = "Address Required.")
     private String recipientAddress;
+    @NotEmpty(message = "Phone Number Required.")
     private String recipientCell;
+    @NotEmpty(message = "Country Required.")
     private String recipientCountry;
+    @NotEmpty(message = "Town Required.")
     private String recipientTown;
 }
--- backend/backend/src/main/java/com/shumbamoney/backend/d.t.o/SenderDto.java
+++ backend/backend/src/main/java/com/shumbamoney/backend/d.t.o/SenderDto.java
@@ -2,18 +2,27 @@ package com.shumbamoney.backend.d.t.o;
 import java.io.Serializable;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
 
 public class SenderDto implements Serializable {
+    @NotEmpty(message = "Name Required.")
     private String senderName;
+    @NotEmpty(message = "Email Required.")
     private String senderEmail;
+    @NotEmpty(message = "Address Required.")
     private String senderAddress;
+    @NotEmpty(message = "Password Required.")
     private String password;
+    @NotEmpty(message = "Phone Number Required.")
     private String senderCell;
+    @NotEmpty(message = "Country Required.")
     private String senderCountry;
+    @NotEmpty(message = "Town Required.")
     private String senderTown;
     private String token;
 }
