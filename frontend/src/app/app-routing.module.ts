f70dec3852a1971cfd4157e420d0dc41b6020a8a
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-09-05 01:12:04 +0200

    adding packages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "recipient",
+    pathMatch: "full",
+  },
+  {
+    path: "",
+    component: DashboardLayoutComponent,
+    canActivate: [AuthGuardService],
+    children: [
+      {
+        path: "",
+        loadChildren: () => import("./layouts/dashboard-layout/dashboard-layout.module").then((m) => m.DashboardLayoutModule),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "",
+        loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
+      },
+    ],
+  },
+  {
+    path: "**",
+    redirectTo: "recipient",
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true,
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { RecipientComponent } from './pages/dashboard/recipient/recipient.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    DashboardLayoutComponent,
+    RecipientComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    
   ],
-  providers: [],
+  providers: [AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
